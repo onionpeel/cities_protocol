56233447861fa385141bdb0dc16532c1c28cb295
Jeff Dinkelman <mfankenstein@yahoo.com>
2021-04-22 23:44:24 -0700

    adds components; updates index.css

---
+++ frontend/src/components/App.js
@@ -0,0 +1,24 @@
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        Knock Knock
+      </div>
+      <div>
+        Who's there?
+      </div>
+      <div>
+        App
+      </div>
+      <div>
+        App who?
+      </div>
+      <div>
+        Appooh named Winnie
+      </div>
+    </div>
+  );
+}
+
+export default App;
--- frontend/src/components/Education.js
+++ frontend/src/components/Education.js
@@ -1,8 +1,12 @@
 const Education = () => {
   return (
     <div>
-      You: Ha ha!  Look at you, component.  You are so boring and lazy.  You have no style and you do nothing.
-      Component:  Let's see who is laughing in a week when I am broken in a hundred different ways.  You will be begging for boring and lazy.
+      <div>
+        You: Ha ha!  Look at you, component.  You are so boring and lazy.  You have no style and you do nothing.
+      </div>
+      <div>
+        Component:  Let's see who is laughing in a week when I am broken in a hundred different ways.  You will be begging for boring and lazy.
+      </div>
     </div>
   );
 };
