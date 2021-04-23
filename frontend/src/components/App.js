7fdbd13657014444a90cba51def10e2513f31496
Jeff Dinkelman <mfankenstein@yahoo.com>
2021-04-23 00:23:03 -0700

    finishes react-router; adds Quiz.js

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,22 +1,64 @@
+import Header from './Header';
+import Footer from './Footer';
+import Home from './Home';
+import Quiz from './Quiz';
+import Proposal from './Proposal';
+import Education from './Education';
+import Metamask from './Metamask';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <div>
-        Knock Knock
-      </div>
-      <div>
-        Who's there?
-      </div>
-      <div>
-        App
-      </div>
-      <div>
-        App who?
-      </div>
-      <div>
-        Appooh named Winnie
-      </div>
+      <Header />
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/quiz">Quiz</Link>
+              </li>
+              <li>
+                <Link to="/proposal">Proposal</Link>
+              </li>
+              <li>
+                <Link to="/education">Education</Link>
+              </li>
+              <li>
+                <Link to="/metamask">Metamask</Link>
+              </li>
+            </ul>
+          </nav>
+
+          <Switch>
+            <Route path="/quiz">
+              <Quiz />
+            </Route>
+            <Route path="/proposal">
+              <Proposal />
+            </Route>
+            <Route path="/education">
+              <Education />
+            </Route>
+            <Route path="/metamask">
+              <Metamask />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+      <br></br>
+      <Footer />
     </div>
   );
 }
