a56dba4056821462642d7f33c4df9f7f888d13b8
Jeff Dinkelman <mfankenstein@yahoo.com>
2021-04-24 15:56:53 -0700

    adds metamask to app.js

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,3 +1,12 @@
+import { useEffect, useState } from 'react';
+import detectEthereumProvider from '@metamask/detect-provider'
+import { ethers } from 'ethers';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import Header from './Header';
 import Footer from './Footer';
 import HomeLocked from './HomeLocked';
@@ -5,14 +14,70 @@ import Quiz from './Quiz';
 import Proposal from './Proposal';
 import Signup from './Signup';
 import Governance from './Governance';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
 
 function App() {
+  let [provider, setProvider] = useState();
+  let [ethersProvider, setEthersProvider] = useState();
+  let [isConnected, setIsConnected] = useState();
+  let [currentMetaMaskAccount, setCurrentMetaMaskAccount] = useState(null);
+
+  useEffect(() => {
+    const init = async () => {
+      setIsConnected(false);
+      //detect whether the browser is connected to a provider
+      let ethereumProvider = await detectEthereumProvider();
+      if (ethereumProvider) {
+        setProvider(ethereumProvider);
+        startApp(ethereumProvider);
+      } else {
+        alert('Please install MetaMask!');
+        return;
+      };
+
+      async function startApp(_ethereumProvider) {
+        //The provider detected by detectEthereumProvider() must be the same as window.ethereum
+        if (_ethereumProvider !== window.ethereum) {
+          alert('Do you have multiple wallets installed?');
+          return;
+        };
+
+        let metamaskAccount;
+        let accounts = await _ethereumProvider.request({ method: 'eth_accounts' });
+          if (accounts.length > 0) {
+            metamaskAccount = accounts[0];
+            setIsConnected(true);
+            setCurrentMetaMaskAccount(accounts[0]);
+          };
+        console.log(`metamaskAccount ${metamaskAccount}`);
+
+        let _ethersProvider = await new ethers.providers.Web3Provider(_ethereumProvider);
+        setEthersProvider(_ethersProvider);
+      };
+    };
+    init();
+  }, []);
+
+  const getAccounts = async () => {
+    const accounts = await provider.request({ method: 'eth_requestAccounts' });
+    await handleAccountsChanged(accounts);
+  };
+
+  function handleAccountsChanged(accounts) {
+    if (accounts.length === 0) {
+      console.log('Please connect to MetaMask.');
+    } else if (accounts[0] !== currentMetaMaskAccount) {
+      console.log('account[0]: ', accounts[0]);
+      setCurrentMetaMaskAccount(accounts[0]);
+      setIsConnected(true);
+      // window.location.reload();
+    }
+  };
+
+  const handleOnConnect = async () => {
+    await getAccounts();
+    provider.on('accountsChanged', handleAccountsChanged);
+  };
+
   return (
     <div className="App">
       <Header />
@@ -48,16 +113,19 @@ function App() {
             <Route path="/signup">
               <Signup />
             </Route>
-            <Route path="/goverance">
+            <Route path="/governance">
               <Governance />
             </Route>
             <Route path="/">
-              <Home />
+              <HomeLocked />
             </Route>
           </Switch>
         </div>
       </Router>
       <br></br>
+      <div>
+        <button onClick={handleOnConnect}>Connect to MetaMask</button>
+      </div>
       <Footer />
     </div>
   );
