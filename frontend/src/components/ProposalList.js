b0ceedc3926c90794e381f3fbdc104b2d9a37433
Jeff Dinkelman <mfankenstein@yahoo.com>
2021-04-27 00:28:01 -0700

    Adds Proposal.js so that individual proposals can be dynamically generated on ProposalList.js

--- frontend/src/components/ProposalList.js
+++ frontend/src/components/ProposalList.js
@@ -1,57 +1,38 @@
 import {Link} from "react-router-dom";
 import {Button, Card, ListGroup} from 'react-bootstrap';
+import Proposal from './Proposal';
+import { proposalArray } from '../DELETEBEFOREPRODUCTION/proposalArray.js';
 
 const ProposalList = () => {
+  const list = proposalArray.map((proposal, i) => (
+    <div key={i}>
+      <Proposal
+        typeOfAction={proposal.typeOfAction}
+        neighborhood={proposal.neighborhood}
+        personInCharge={proposal.personInCharge}
+        description={proposal.description}
+        expiration={proposal.expiration}
+        budget={proposal.budget}
+        taroToVote={proposal.taroToVote}
+      />
+    </div>
+  ));
+
   return (
     <div>
       <div>
         <Link to="/createproposal">Create a proposal</Link>
       </div>
-      <div>
-        This will be a list of proposals
-      </div>
 
       <main className="m-4">
-        <Card className="gray mb-4">
-          <Card.Body>
-            <Card.Text>
-              TARO in wallet
-            </Card.Text>
-            <Card.Title className="text-large">Locked</Card.Title>
-            <Card.Text>
-              TARO to harvest
-            </Card.Text>
-            <Button disabled block>Claim Tokens</Button>
-          </Card.Body>
-        </Card>
-        <Card className="gray mb-4">
-          <Card.Body>
-            <Card.Text>
-              TARO in wallet
-            </Card.Text>
-            <Card.Title className="text-large">Locked</Card.Title>
-            <Card.Text>
-              TARO to harvest
-            </Card.Text>
-            <Button disabled block>Claim Tokens</Button>
-          </Card.Body>
-        </Card>
-        <Card className="gray mb-4">
-          <Card.Body>
-            <Card.Text>
-              Proposals
-            </Card.Text>
-            <Card.Title className="text-large">Locked</Card.Title>
-            <Button disabled block>Vote</Button>
-          </Card.Body>
-        </Card>
-          <ListGroup>
-            <ListGroup.Item>Cras justo odio</ListGroup.Item>
-            <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
-            <ListGroup.Item>Morbi leo risus</ListGroup.Item>
-            <ListGroup.Item>Porta ac consectetur ac</ListGroup.Item>
-            <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
-          </ListGroup>
+        {list}
+        <ListGroup>
+          <ListGroup.Item>Cras justo odio</ListGroup.Item>
+          <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
+          <ListGroup.Item>Morbi leo risus</ListGroup.Item>
+          <ListGroup.Item>Porta ac consectetur ac</ListGroup.Item>
+          <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
+        </ListGroup>
       </main>
     </div>
   );
