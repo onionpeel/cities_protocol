7d51a6e3cafb99cd706bd1e441b674e9a16939e1
Jeff Dinkelman <mfankenstein@yahoo.com>
2021-04-30 23:47:19 -0700

    creates ValidationRequired.js alert and ValidationRequiredContext.js; creates a context in App.js; the value for this context is set in Home.js and is consumed in ProposalList.js

---
+++ frontend/src/alerts/ValidationRequired.js
@@ -0,0 +1,13 @@
+import { Alert } from 'react-bootstrap';
+
+const ValidationRequired = () => {
+  return (
+    <div>
+      <Alert variant="secondary">
+        Please validate your account so you can create and vote on proposals.  LINK TO QUIZ PAGE
+      </Alert>
+    </div>
+  );
+};
+
+export default ValidationRequired;
